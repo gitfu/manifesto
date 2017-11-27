6b2e1134cd21582bee16ec8ce45b8712484ad31e
Leroy Scandal <gitfu@users.noreply.github.com>
2017-11-26 21:06:15 -0500

    Update manifesto.go

--- manifesto.go
+++ manifesto.go
@@ -63,7 +63,7 @@ func (j *Job) mkFlags() {
 	flag.StringVar(&j.SubFile, "s", "", "subtitle file to segment (optional)")
 	flag.StringVar(&j.TopLevel, "d", "", "override top level directory for hls files (optional)")
 	flag.StringVar(&j.JasonFile, "j", `./hls.json`, "JSON file of variants (optional)")
-	j.AddSubs = flag.Bool("no-subs", false, "do not add subtitles")
+	*j.AddSubs = flag.Bool("no-subs", false, "do not add subtitles")
 	flag.StringVar(&j.CmdTemplate, "t", `./cmd.template`, "command template file (optional)")
 	flag.StringVar(&j.UrlPrefix, "u", "", "url prefix to add to index.m3u8 path in master.m3u8 (optional)")
 	flag.Parse()
